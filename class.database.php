8fec0b6b7179b1791778a4b2ecf4fb096c4f1f5a
Vivek V <vivekv@vivekv.com>
2014-04-17 01:00:51 -0400

    Version bumped to v1.2.8a

--- class.database.php
+++ class.database.php
@@ -7,7 +7,7 @@
  * @author    Vivek V <vivekv@vivekv.com>
  * @copyright Copyright (c) 2013
  * @license   http://opensource.org/licenses/gpl-3.0.html GNU Public License
- * @version   1.2.8
+ * @version   1.2.8a
  **/
 
 class Database
